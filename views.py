6ff04def114fdf80ece775d733a4ebbe8827c4bd
Peter Inglesby <peter.inglesby@gmail.com>
2021-10-31 21:15:01 +0000

    Support lead paragraphs

--- views.py
+++ views.py
@@ -4,14 +4,21 @@ from markdown import markdown
 
 
 def home(request):
-    with open("pages/home.md") as f:
-        news = mark_safe(markdown(f.read()))
-    ctx = {"news": news}
+    content = _load_markdown("pages/home.md")
+    ctx = {"content": content}
     return render("request", "home.html", ctx)
 
 
 def biography(request):
-    with open("pages/biography.md") as f:
-        content = mark_safe(markdown(f.read()))
+    content = _load_markdown("pages/biography.md")
     ctx = {"page_id": "biography", "content": content}
     return render("request", "page.html", ctx)
+
+
+def _load_markdown(path):
+    with open(path) as f:
+        md = f.read()
+    raw_html = markdown(md)
+    # There's probably a better way of doing this via a markdown extension...
+    html = raw_html.replace("<p>!", '<p class="lead">')
+    return mark_safe(html)
