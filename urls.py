731686b1eb73569447d52bf8b7dc743aab4e0525
Peter Inglesby <peter.inglesby@gmail.com>
2021-11-01 20:27:37 +0000

    Add links to navbar

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 import views
 
 urlpatterns = [
-    path("", views.home),
-    path("biography/", views.biography),
-    path("concerts/", views.concerts),
+    path("", views.home, name="home"),
+    path("biography/", views.biography, name="biography"),
+    path("concerts/", views.concerts, name="concerts"),
 ]
